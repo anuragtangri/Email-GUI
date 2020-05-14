7e3144df9c08286690bb04f611d888056d4de025
Anurag Tangri <anuragtangri321@gmail.com>
2020-05-14 23:57:22 +0530

    Update EMAIL_GUI.py

--- EMAIL_GUI.py
+++ EMAIL_GUI.py
@@ -1,11 +1,22 @@
 import  tkinter as tk
-from functools import partial
 import yagmail
+import  sys
+
+def send_email():
+    send_e=e1.get()
+    pass_e=e2.get()
+    rec_e=e3.get()
+    #print(send_e + pass_e + rec_e)
+    yag_smtp_connection = yagmail.SMTP(user=send_e, password=pass_e, host='smtp.gmail.com')
+    subject = 'Hello from Anurag ! '
+    contents = ['Hello tom this is ANURAG speaking . Hows it going in JALANDHAR ?']
+    # send the email
+    yag_smtp_connection.send(rec_e, subject, contents)
+    sys.exit()
 
 
 window=tk.Tk()
-window.geometry("600x400")
-
+window.geometry("400x200")
 
 sender_email=tk.StringVar()
 sender_password=tk.StringVar()
@@ -15,7 +26,7 @@ label1=tk.Label(text="Sender Email Address")
 label1.grid(row=0,column=0)
 
 e1=tk.Entry(window,textvariable=sender_email)
-e1.grid(row=0,column=1)
+e1.grid(row=0,column=1,padx=10,pady=10)
 
 
 label2=tk.Label(text="Sender Password")
@@ -24,7 +35,7 @@ label2.grid(row=1,column=0)
 
 
 e2=tk.Entry(window,textvariable=sender_password)
-e2.grid(row=1,column=1)
+e2.grid(row=1,column=1,padx=10,pady=10)
 
 
 
@@ -32,24 +43,11 @@ label3=tk.Label(text="Receiver Email")
 label3.grid(row=2,column=0)
 
 e3=tk.Entry(window,textvariable=receiver_email)
-e3.grid(row=2,column=1)
+e3.grid(row=2,column=1,padx=10,pady=10)
 
 
-def show():
-    send_e=e1.get()
-    pass_e=e2.get()
-    rec_e=e3.get()
-    #print(send_e + pass_e + rec_e)
-    yag_smtp_connection = yagmail.SMTP(user=send_e, password=pass_e, host='smtp.gmail.com')
-    subject = 'Hello from Anurag'
-    contents = ['Hello tom this is ANURAG speaking']
-    # send the email
-    yag_smtp_connection.send(rec_e, subject, contents)
-
-
-
-button_1=tk.Button(window,text="SEND",command=show)
-button_1.grid(row=3,column=0)
+button_1=tk.Button(window,text="SEND",command=send_email)
+button_1.grid(row=3,column=1)
 
 
 window.mainloop()
